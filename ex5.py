18fc98ebcbe691cc5b9f61facd030279fe6c6392
rituraj <riturajreso@gmail.com>
2018-07-18 19:15:46 +0530

    Local Commit

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Ritu Raj'
+my_age = 25
+my_height = 68 #inches
+my_weight = 70 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." %my_name
+print "He's %d inches talls." %my_height
+print "He's %d KG heavy." %my_weight
+print "He's got %s eyes and %s teeth." %(my_eyes,my_teeth)
+print "His hair color is %r." %my_hair
+   
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age 
++ my_height + my_weight) 
