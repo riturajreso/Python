616804bceaaf4da2cded2b13730653fa1127a7c6
riturajreso <riturajreso@gmail.com>
2018-07-24 23:35:48 +0530

    local-6

---
+++ ex15.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here is you file %r" %filename
+print "-"*10
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+txt_again = open(file_again)
+print txt_again.read()
