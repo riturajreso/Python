b448ac11fa0acf891b1cb13fcddc633cf8175e06
riturajreso <riturajreso@gmail.com>
2018-07-24 15:02:27 +0530

    local-4

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the no of days", days 
+print "Here are the no of Months", months
+
+
+print """
+There's somthing going on here.
+With three double-quotes.
+We'll be able to type as much as we like
+Even 4 lines if we want, 5 or 6.
+"""
