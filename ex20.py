c5c8c226aef31816cf3846ab647def257b13e263
rituraj <riturajreso@gmail.com>
2018-08-02 15:18:20 +0530

    Offical -7

--- ex20.py
+++ ex20.py
@@ -11,10 +11,13 @@ def rewind(f):
 def print_a_line(line_count,f):
 	print line_count, f.readline()
 
+#Using same variable same file obj doesn't return something 
 current_file = open(input_file)
+current_file2 = open(input_file)
 
-print "First let's print the whole file\n."
-print_all(current_file)
+print "First let's print the whole file using %r\n." % scritp
+
+print_all(current_file2)
 
 print "Printing lines"
 line_no = 1;
